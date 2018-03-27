56375c69ac5e539e8805533d0f99c7da2da634bb
Vinchurkar <siddhantvinchurkar@gmail.com>
2018-03-27 21:35:21 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE HTML>
+<!--
+	Aerial by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html lang="en">
+	<head>
+		<title>Arctic Impala</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="theme-color" content="#29b6f6" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="shortcut icon" href="assets/css/images/favicon.png" type="image/x-icon">
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="loading">
+		<div id="wrapper">
+			<div id="bg"></div>
+			<div id="overlay">
+<video poster="media/iamai.png" width="50%" height="50%" id="vid" style="margin: auto; width: 100%; height:100%;" autoplay loop>
+<source src="media/iamai.mp4" type="video/mp4">
+	Unable to fetch video
+</video></div>
+			<div id="main">
+
+				<!-- Header -->
+					<header id="header">
+						<h1>Arctic Impala</h1>
+						<p>Welcome <a href="#" class="icon fa-home"><span class="label">home</span> &nbsp;home!</a> &nbsp;&bull;&nbsp; Don't bother the <a href="#" class="icon fa-code"><span class="label">programmers</span>&nbsp;programmers</a> at work.</p>
+						<nav>
+							<ul>
+<li><a href="#" id="play_toggle" class="icon fa-pause" title="Play / Pause"><span class="label">Play / Pause</span></a></li>
+<li><a href="#" id="myBtn" class="icon fa-info" title="General Information"><span class="label">General Information</span></a></li><li><a href="https://mail.arcticimpala.tk/" target="_blank" class="icon fa-envelope" title="Arctic Mail"><span class="label">Arctic Mail</span></a></li>
+								<li><a href="http://192.168.0.110:5000/" target="_blank" class="icon fa-film" title="Streama"><span class="label">Streama</span></a></li>
+<li><a href="https://owncloudai.tk/" target="_blank" class="icon fa-cloud" title="Owncloud"><span class="label">Owncloud</span></a></li>
+								<li><a href="http://onyxai.cf/" target="_blank" class="icon fa-dot-circle-o" title="Onyx"><span class="label">Onyx</span></a></li>
+<li><a href="http://arcticphoton.tk:8080/" target="_blank" class="icon fa-bolt" title="Arctic Photon"><span class="label">Arctic Photon</span></a></li>
+								<li><a href="http://arcticimpala.tk:61208/1" target="_blank" class="icon fa-server" title="Geeky Server Stats"><span class="label">Geeky Server Stats</span></a></li><li><a href="https://arctic-ssh.tk" onclick="window.open('https://arctic-ssh.tk', 'newwindow', 'width=728,height=440'); return false;" class="icon fa-code" title="Terminal For Noobs"><span class="label">Terminal</span></a></li><li><a href="http://arctic-ssh.tk:2222" onclick="window.open('http://arctic-ssh.tk:2222', 'newwindow', 'width=728,height=440'); return false;" class="icon fa-terminal" title="Terminal For Experts"><span class="label">Terminal</span></a></li>
+							</ul>
+						</nav>
+<iframe width="40%" style="margin-top: 70px;" height="100" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/363011750&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe>
+
+					</header>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<span class="copyright">&copy; 2017 <a href="http://volatile.cf/">Volatile, Inc.</a> Designed by <a href="https://blog.siddhantvinchurkar.me/">Siddhant Vinchurkar</a>.</span>
+					</footer>
+
+			</div>
+		</div>
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+<div class="card">
+<span class="close">&times;</span>
+  <img src="assets/css/images/ai.png" alt="Avatar" style="width:100%">
+  <div class="container" style="color: #000000;">
+    <h4 style="font-weight: bold;">Welcome to our humble abode; welcome to Arctic Impala!</h4> 
+    <h4 style="font-weight: bold;">It doesn't look like anyone's here. What do I do?</h4> 
+    <p>There's so many things to do! You could</p>
+    <p>&bull; Connect to the WiFi network</p>
+    <p>&bull; Drink something</p> 
+    <p>&bull; Play music or watch something</p>
+    <p>&bull; Eat, cook or order some food</p>
+<h4 style="font-weight: bold;">Alright, how do I connect to the WiFi?</h4> 
+    <p>There are three WiFi networks in the house &rarr;</p>
+    <p>&bull; Arctic Impala: Long range guest network. Password: bazzinga</p>
+    <p>&bull; This is Sparta!!!: Long range resident network. Password: ArcticImpala199X</p>
+    <p>&bull; A Wifi Has No Name: Short range (but much faster) resident network. Password: 06711658</p>
+    <p style="font-style: italic;">Do not connect to resident networks if your stay in Arctic Imapla is less than 24 hours. Thanks! :)</p>
+<h4 style="font-weight: bold;">What can you offer me to drink?</h4> 
+    <p>There's usually some chilled beer and orange juice in the refrigerator. There's also a coffee machine in the kitchen and you can find some coffee powder in the rightmost cabinet. Help yourself.</p>
+<h4 style="font-weight: bold;">Okay, what entertainment options do I have?</h4> 
+    <p>Play some music. There's three guitars and a bluetooth speaker lying around somewhere in the house. You could even watch a TV show or a movie on <a href="http://192.168.0.110:5000/" target="_blank">Streama</a>.</p>
+<h4 style="font-weight: bold;">I'm hungry/bored. What can you offer me to eat?</h4> 
+    <p>Lots. There's always fruits, vegetables, bread, eggs, idly/dosa batter, butter, cheese, chicken and sometimes chicken sausages in the fridge. If you're lucky, you might find some leftovers too. In the bottom cabinets in the kitchen, you will find spaghetti, macroni, milk and instant noodles. All spices, seasoning and condiments are stored in the top cabinets. Oil, salt/pepper, rice and corn flakes / chocos are usually left on the counter. If you're cooking something simple, use the induction cooktop. It's much faster.</p>
+<h4 style="font-weight: bold;"><br />The residents of Arctic Impala hope you have a pleasent stay. Feel free to ask them if you need anything. :)<br /></h4> 
+  </div>
+</div>
+</div>
+</div>
+<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+		<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+		<script>
+			window.onload = function() { document.body.className = ''; }
+			window.ontouchmove = function() { return false; }
+			window.onorientationchange = function() { document.body.scrollTop = 0; }
+		</script>
+<script>
+var video = document.getElementById("vid");
+var button = document.getElementById("play_toggle");
+button.onclick = function() {playpause()}
+function playpause(){
+if(!video.paused){
+    video.pause();
+    video.style.display = 'none';
+    button.setAttribute("class", "icon fa-play");
+}
+else {
+video.play();
+video.style.display = 'block';
+    button.setAttribute("class", "icon fa-pause");
+}
+}
+</script>
+	</body>
+</html>
